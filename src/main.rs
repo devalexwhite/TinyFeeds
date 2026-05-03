e3d55ce73c83c22f67f2b3da56f3a6ef222061ea
Alex White <hi@thatalexguy.dev>
2026-05-03 11:11:32 -0400

    Size optimizations, article rendering
    I think I'm *finally* getting a hang of this

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,52 @@ use std::{
 };
 
 use chrono::Datelike;
+use frostmark::{MarkState, MarkWidget};
+use iced::{
+    Element, Task,
+    application::timed::UpdateFn,
+    widget::{column, container, scrollable, text},
+};
 use rss::Channel;
 
-#[tokio::main]
-async fn main() {
-    let mut app = App::new();
-    app.fetch_stories().await;
+fn main() -> iced::Result {
+    iced::application(App::new, App::update, App::view).run()
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Story {
     author: String,
     url: String,
     html: String,
 }
 
+#[derive(Debug, Clone)]
+enum Message {
+    FetchStories,
+    SetStories(Vec<Story>),
+}
+
 struct App {
     feeds: Vec<String>,
     stories: Vec<Story>,
+    mark_state: MarkState,
 }
 
 impl App {
-    fn new() -> Self {
+    fn new() -> (Self, Task<Message>) {
         if let Some(home) = home_dir() {
             let mut feeds_file_path = home.clone();
             feeds_file_path.push(".config/tinyfeeds/feeds.txt");
 
             if let Ok(true) = fs::exists(feeds_file_path.as_path()) {
-                return App {
-                    feeds: file_lines(feeds_file_path.as_path()),
-                    stories: Vec::new(),
-                };
+                return (
+                    App {
+                        feeds: file_lines(feeds_file_path.as_path()),
+                        stories: Vec::new(),
+                        mark_state: MarkState::with_html(""),
+                    },
+                    Task::done(Message::FetchStories),
+                );
             }
 
             let mut feeds_base_path = home.clone();
@@ -47,44 +62,46 @@ impl App {
             File::create(feeds_file_path).expect("Failed to create config file.");
         }
 
-        App {
-            feeds: Vec::new(),
-            stories: Vec::new(),
+        (
+            App {
+                feeds: Vec::new(),
+                stories: Vec::new(),
+                mark_state: MarkState::with_html(""),
+            },
+            Task::done(Message::FetchStories),
+        )
+    }
+
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::FetchStories => {
+                Task::perform(fetch_stories(self.feeds.clone()), Message::SetStories)
+            }
+            Message::SetStories(stories) => {
+                self.stories = stories.clone();
+                self.mark_state = MarkState::with_html(self.stories[0].html.clone().as_str());
+                Task::none()
+            }
         }
     }
 
-    async fn fetch_stories(&mut self) {
-        let today = chrono::Local::now();
-
-        for feed in self.feeds.clone() {
-            if let Ok(feed_content) = reqwest::get(feed).await {
-                if let Ok(feed_bytes) = feed_content.bytes().await {
-                    if let Ok(channel) = Channel::read_from(&feed_bytes[..]) {
-                        for story in channel.items {
-                            if let Some(pub_date) = story.pub_date {
-                                let pub_date_c =
-                                    chrono::DateTime::parse_from_rfc2822(pub_date.clone().as_str())
-                                        .unwrap();
-
-                                if pub_date_c.year() != today.year()
-                                    || pub_date_c.month() != today.month()
-                                    || pub_date_c.day() != today.day()
-                                {
-                                    continue;
-                                }
-
-                                self.stories.push(Story {
-                                    author: channel.title.clone(),
-                                    url: story.link.unwrap_or(String::from("")),
-                                    html: story.description.unwrap_or(String::from("")),
-                                });
-                            }
-                        }
-                    }
-                }
-            }
+    fn view(&self) -> Element<'_, Message> {
+        if self.stories.len() == 0 {
+            container(text("Loading Feeds...").size(30))
+                .padding(10)
+                .center(800)
+                .style(container::rounded_box)
+                .into()
+        } else {
+            container(scrollable(column![
+                MarkWidget::new(&self.mark_state),
+                "A big meow",
+            ]))
+            .padding(10)
+            .center(800)
+            .style(container::rounded_box)
+            .into()
         }
-        println!("{:?}", self.stories);
     }
 }
 
@@ -94,4 +111,40 @@ fn file_lines(filename: impl AsRef<Path>) -> Vec<String> {
     buf.lines()
         .map(|l| l.expect("Could not read line."))
         .collect()
+}
+
+async fn fetch_stories(feeds: Vec<String>) -> Vec<Story> {
+    let today = chrono::Local::now();
+    let mut stories = Vec::new();
+
+    for feed in feeds {
+        if let Ok(feed_content) = reqwest::get(feed).await {
+            if let Ok(feed_bytes) = feed_content.bytes().await {
+                if let Ok(channel) = Channel::read_from(&feed_bytes[..]) {
+                    for story in channel.items {
+                        if let Some(pub_date) = story.pub_date {
+                            let pub_date_c =
+                                chrono::DateTime::parse_from_rfc2822(pub_date.clone().as_str())
+                                    .unwrap();
+
+                            if pub_date_c.year() != today.year()
+                                || pub_date_c.month() != today.month()
+                                || pub_date_c.day() != today.day()
+                            {
+                                continue;
+                            }
+
+                            stories.push(Story {
+                                author: channel.title.clone(),
+                                url: story.link.unwrap_or(String::from("")),
+                                html: story.description.unwrap_or(String::from("")),
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    stories
 }
