f2732d9c06a76aa0c5a1896f38b88fce011f8f15
Alex White <hi@thatalexguy.dev>
2026-05-29 14:15:26 -0400

    GTK rebuild

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 # Build a smol boy binary
 
 cargo build --release
-upx --best --lzma target/release/tinyfeeds
+if command -v upx &> /dev/null; then
+    upx --best --lzma target/release/TinyFeeds
+else
+    echo "upx not found, skipping compression."
+fi
